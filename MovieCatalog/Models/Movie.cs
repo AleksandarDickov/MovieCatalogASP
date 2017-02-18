18259274907040a24c9d7c600aeeaa865c705920
Zdravka <zdravka92@hotmail.com>
2017-02-18 14:20:49 +0100

    ograni;enja karaktera

--- MovieCatalog/Models/Movie.cs
+++ MovieCatalog/Models/Movie.cs
@@ -8,13 +8,24 @@ namespace MovieCatalog.Models
 {
     public class Movie
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Director { get; set; }
 
+        [Required]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Range(typeof(DateTime), "1/1/1920", "1/1/2020", ErrorMessage ="Date is out of Range")]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
 
     }
