8d6e74697beb81793a54fe8d5b153a554c18c599
Aleksandar <dicckov1992@gmail.com>
2017-02-25 11:14:17 +0100

    gotovo sortiranje

--- MovieCatalog/Controllers/MoviesController.cs
+++ MovieCatalog/Controllers/MoviesController.cs
@@ -16,47 +16,48 @@ namespace MovieCatalog.Controllers
 
         // GET: Movies
 
-        public ActionResult Index(string searchString , string sortOrder)
+        public ActionResult Index(string searchString, string sortOrder)
         {
-                ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-                ViewBag.GenreSortParm = String.IsNullOrEmpty(sortOrder) ? "genre_desc" : "";
-                ViewBag.DirectorSortParm = String.IsNullOrEmpty(sortOrder) ? "director_desc" : "";
-                ViewBag.ReleaseDateSortParm = sortOrder == "ReleaseDate" ? "date_desc" : "ReleaseDate";
-                var movies = from m in db.Movies select m;
+            ViewBag.NameSortParm = sortOrder == "Name_asc" ? "Name_desc" : "Name_asc";
+            ViewBag.GenreSortParm = sortOrder == "Genre_asc" ? "Genre_desc" : "Genre_asc";
+            ViewBag.DirectorSortParm = sortOrder == "Director_asc" ? "Director_desc" : "Director_asc";
+            ViewBag.ReleaseDateSortParm = sortOrder == "Date_asc" ? "Date_desc" : "Date_asc";
+            var movies = from m in db.Movies select m;
 
-                if (!String.IsNullOrEmpty(searchString))
-                {
-                    movies = movies.Where(s => s.Name.StartsWith(searchString) || s.Genre.StartsWith(searchString));
-                }
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Name.StartsWith(searchString) || s.Genre.StartsWith(searchString));
+            }
 
-                switch (sortOrder)
-                {
-                    case "name_desc":
-                        movies = movies.OrderByDescending(s => s.Name);
-                        break;
-                    case "director_desc":
-                        movies = movies.OrderByDescending(s => s.Director);
-                        break;
-                    case "Director":
-                        movies = movies.OrderBy(s => s.Director);
-                        break;
-                    case "genre_desc":
-                        movies = movies.OrderByDescending(s => s.Genre);
-                        break;
-                    case "Genre":
-                        movies = movies.OrderBy(s => s.Genre);
-                        break;
-                    case "ReleaseDate":
-                        movies = movies.OrderBy(s => s.ReleaseDate);
-                        break;
-                    case "date_desc":
-                        movies = movies.OrderByDescending(s => s.ReleaseDate);
-                        break;
-                    default:
-                        movies = movies.OrderBy(s => s.Name);
-                        break;
-                }
-            
+            switch (sortOrder)
+            {
+                case "Name_desc":
+                    movies = movies.OrderByDescending(s => s.Name);
+                    break;
+                case "Director_desc":
+                    movies = movies.OrderByDescending(s => s.Director);
+                    break;
+                case "Director_asc":
+                    movies = movies.OrderBy(s => s.Director);
+                    break;
+                case "Genre_desc":
+                    movies = movies.OrderByDescending(s => s.Genre);
+                    break;
+                case "Genre_asc":
+                    movies = movies.OrderBy(s => s.Genre);
+                    break;
+                case "Date_asc":
+                    movies = movies.OrderBy(s => s.ReleaseDate);
+                    break;
+                case "Date_desc":
+                    movies = movies.OrderByDescending(s => s.ReleaseDate);
+                    break;
+                case "Name_asc":
+                default:
+                    movies = movies.OrderBy(s => s.Name);
+                    break;
+            }
+
             return View(movies.ToList());
         }
 
